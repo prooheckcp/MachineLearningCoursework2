c39b19418c49ba8649add713b781bc0a7e2e477b
Vasco Soares (Prooheckcp) <vasco.soares.2001@gmail.com>
2022-04-25 21:05:58 +0100

    Getting test and train data

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 # Imports
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
+# Constants
+TEST_DATA_PERCENTAGE = 20  # Value in percentage %
+USELESS_COLUMNS = ["id", "wind_speed", "atmo_opacity"]
+ATTRIBUTE_COLUMNS = ["terrestrial_date", "sol", "ls", "month"]
+GOAL_COLUMNS = ["min_temp", "max_temp", "pressure"]
+
+# Variables
 dataFrame = pd.read_csv("mars-weather.csv")
+
+# Clear the dataFrame
+filteredData = dataFrame.drop(columns=USELESS_COLUMNS)
+filteredData.dropna(inplace=True) # Remove incomplete data
+
+X = filteredData.drop(columns=GOAL_COLUMNS)
+Y = filteredData.drop(columns=ATTRIBUTE_COLUMNS)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=TEST_DATA_PERCENTAGE / 100)
