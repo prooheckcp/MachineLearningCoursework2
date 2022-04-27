9e31aa7cfebe247dc53ab4011836739214254d39
Vasco Soares (Prooheckcp) <vasco.soares.2001@gmail.com>
2022-04-28 00:56:38 +0100

    pre-process terrestrial date data

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import OneHotEncoder
 # Constants
 TEST_DATA_PERCENTAGE = 20  # Value in percentage %
 USELESS_COLUMNS = ["id", "wind_speed", "atmo_opacity"]
-ATTRIBUTE_COLUMNS = ["terrestrial_date", "sol", "ls", "month"]
+ATTRIBUTE_COLUMNS = ["sol", "ls", "month", "terrestrial_month", "terrestrial_year", "terrestrial_quarter"]
 GOAL_COLUMNS = ["min_temp", "max_temp", "pressure"]
 MODELS_DICTIONARY = {
     "Logistic regression model": KNeighborsRegressor()
@@ -24,10 +24,19 @@ filteredData.dropna(inplace=True) # Remove incomplete data
 # Convert the month column into an enum
 filteredData["month"] = filteredData["month"].rank(method="dense", ascending=True).astype(int)
 
+# Pre-process terrestrial date
+filteredData["terrestrial_date"] = pd.to_datetime(filteredData["terrestrial_date"]) # Convert to dateTime64
+
+date = filteredData["terrestrial_date"]
+filteredData["terrestrial_month"] = date.dt.month
+filteredData["terrestrial_year"] = date.dt.year
+filteredData["terrestrial_quarter"] = date.dt.quarter
+
+del filteredData["terrestrial_date"] # Drop the column as it won't be used any longer
+
 X = filteredData.drop(columns=GOAL_COLUMNS)
 Y = filteredData.drop(columns=ATTRIBUTE_COLUMNS)
 
-
 model = KNeighborsRegressor()
 
-#model.fit(X, Y)
+model.fit(X, Y)
